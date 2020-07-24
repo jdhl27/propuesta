bfffa8315b5b91414ed33a7c1cb89ab1e435a4ad
Juan David Hernández <>
2020-07-24 08:09:22 -0500

    agregando mejoras

--- home.js
+++ home.js
@@ -1,25 +1,37 @@
 window.onload=function()
-  {
+{
+  var question;
+  swal("Ingrese la pregunta", {
+    content: "input",
+    closeOnClickOutside: false,
+  })
+  .then((value) => {
+    question = value;
+    const pregunta = document.getElementById('question_html');
+    pregunta.innerHTML = "¿" + question + "?";
+  });
 
-    var screen_windth= window.screen.width;
-    var screen_height= window.screen.height;
+
+  var screen_windth= window.screen.width;
+  var screen_height= window.screen.height;
 
 
-    var boton = document.getElementById("boton");
-    boton.onmouseover = function(e) {
-      var win = Math.floor(Math.random() * ((screen_windth - 200) - 20 )) + 20;
-      var hei = Math.floor(Math.random() * ((screen_height - 200) - 20 )) + 20;
+  var boton = document.getElementById("boton");
+  boton.onmouseover = function(e) {
+    var win = Math.floor(Math.random() * ((screen_windth - 300) - 20 )) + 20;
+    var hei = Math.floor(Math.random() * ((screen_height - 300) - 20 )) + 20;
+    
+    boton.style.left = 0;
+    boton.style.top = 0;
+    
+    boton.style.top = hei + "px";
+    
+    boton.style.left = win + "px";
       
-      boton.style.left = 0;
-      boton.style.top = 0;
-      
-      boton.style.top = hei + "px";
-      
-      boton.style.left = win + "px";
-      
-      
-    };
-  }
+  };
+}
+
+
   
 function aceptar() {
   var rocket = document.getElementById("rocket");
@@ -37,5 +49,7 @@ function aceptar() {
   setTimeout(function(){ 
     rocket.style.top = "67vh";
     rocket.style.left = "65vh";
+    boton.style.top = "41vh";
+    boton.style.left = "52vw";
   }, 6000);
 }
